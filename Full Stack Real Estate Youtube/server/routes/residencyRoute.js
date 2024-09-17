29bfbd601151b19b0cf412bfa79f5d21528fe9a7
kshitij-github <kshitijlamsoge@gmail.com>
2024-09-18 02:48:19 +0530

    PG Diploma Project

--- Full Stack Real Estate Youtube/server/routes/residencyRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createResidency, getAllResidencies, getResidency } from '../controllers/resdCntrl.js';
-const router = express.Router();
-
-router.post("/create", createResidency)
-router.get("/allresd", getAllResidencies)
-router.get("/:id",getResidency)
-
-export { router as residencyRoute}
